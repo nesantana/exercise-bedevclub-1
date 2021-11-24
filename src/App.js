6ec8784c5e58185a25d5293927675b0b933fa7ff
Mateus Santana <mateussantana@Mateus-MacBook-Pro.local>
2021-11-24 17:51:45 -0400

    Construindo Exercicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #EEEEEE;
+`
+
+const BoxShadow = styled.div`
+  width: 450px;
+  height: 450px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, .16);
+  background: #FFFFFF;
+  padding: 20px;
+
+  input {
+    height: 50px;
+    padding: 0 15px;
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  button {
+    height: 50px;
+    width: 100%;
+    background: #EEAA00;
+    border: 0;
+    outline: none;
+    cursor: pointer;
+  }
+`
+
+
+const App = () =>  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <BoxShadow>
+        <input placeholder="Insira seu nome"/>
+        <button>Mostrar nome</button>
+      </BoxShadow>
+    </Container>
   );
 }
 
