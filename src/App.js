b3b79b5c56d2654d4781f310444026f1cec556f2
Mateus Santana <mateussantana@Mateus-MacBook-Pro.local>
2021-11-24 19:35:14 -0400

    Atualizando descricao

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -31,15 +32,32 @@ const BoxShadow = styled.div`
     outline: none;
     cursor: pointer;
   }
+
+  span {
+    padding: 15px;
+    margin-top: 30px;
+    border-radius: 10px;
+    border: 2px solid #999999;
+    display: block;
+  }
 `
 
 
 const App = () =>  {
+  const [name, setName] = useState('')
+  const [show, setShow] = useState(false)
+
+  // Observe que o setShow e o show não estão sendo utilizados,
+  // possívelmente a solução seja utiliza-los!
+  
   return (
     <Container>
       <BoxShadow>
-        <input placeholder="Insira seu nome"/>
+        <input placeholder="Insira seu nome" onChange={(event) => setName(event.target.value)} />
         <button>Mostrar nome</button>
+        <span>
+          {name ? name : 'Comece a digitar'}
+        </span>
       </BoxShadow>
     </Container>
   );
